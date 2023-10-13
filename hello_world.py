6f24da573a8b3a43e74a4f61b33c280c2bb1e202
Michael Kahr <45453344+m-kahr@users.noreply.github.com>
2023-10-13 11:29:43 +0200

    changed hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ print("hello world")
 # I added a new comment
 
 print("fooooooo")
-print("I added some new line in michael's branch")
+print("I changed something which was different in the main branch")
 print("I added a new line from my local clone")
 print("try to create a pull request from VScode")
 print("try another pull request from VScode")
